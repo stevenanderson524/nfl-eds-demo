290a4d778343854b0974ce4bf214c3ecf63aa5eb
Steven Anderson <standers@Stevens-MacBook-Pro-2.local>
2026-04-03 17:16:18 -0700

    Restore full boilerplate scripts.js with daPreview

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -187,3 +187,9 @@ async function loadPage() {
 }
 
 loadPage();
+
+// DA Live Preview
+(async function loadDa() {
+  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
+  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
+}());
